9dd3948f251b1a08125d837433ffeedb80af4e5b
stefanncs27 <stefannicolae.neacsu@ulbsibiu.ro>
2025-03-31 22:59:22 +0300

    persoane

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         String fisier = "src/cuvinte.txt";
+        String fisierPersoane = "src/persoane.txt";
         Map<String, Integer> contor = new HashMap<>();
+        Map<String, Integer> contorPersoane = new HashMap<>();
 
         try {
             File file = new File(fisier);
@@ -25,6 +27,29 @@ public class Main {
             }
             for (String word : contor.keySet()) {
                 System.out.println(word + " " + contor.get(word));
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("eroare");
+        }
+
+        try {
+            File filePersoane = new File(fisierPersoane);
+            Scanner scPersoane = new Scanner(filePersoane);
+            while (scPersoane.hasNext()) {
+                String line = scPersoane.nextLine().trim();
+                if (!line.isEmpty()) {
+                    if (!contorPersoane.containsKey(line)) {
+                        contorPersoane.put(line, 1);
+                    } else {
+                        contorPersoane.put(line, contorPersoane.get(line) + 1);
+                    }
+                }
+            }
+            for (String person : contorPersoane.keySet()) {
+                if (contorPersoane.get(person) > 3) {
+                    System.out.println(person + " apare de " + contorPersoane.get(person) + " ori");
+                }
             }
 
         } catch (FileNotFoundException e) {
